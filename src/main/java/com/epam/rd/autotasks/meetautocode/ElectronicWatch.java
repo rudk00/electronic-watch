e1854b8a6bca06a1a4b93ea1220002b175e957cb
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-10-27 11:40:56 +0300

    Working Electronic Watch

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,7 +6,15 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        int sec = seconds % 3600 % 60;
+        int min = seconds % 3600 / 60;
+        int hours = seconds / 3600 % 24;
+
+        String strSec = (sec < 10) ? "0" + Integer.toString(sec) : Integer.toString(sec);
+        String strMin = (min < 10) ? "0" + Integer.toString(min) : Integer.toString(min);
+
+        System.out.println(hours + ":" + strMin + ":" + strSec);
+    }
 
 
     }
-}
